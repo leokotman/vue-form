24ccc49aec34bc238bb225556f47939bf22bb8f9
levkotman <leoktmn@yandex.ru>
2021-08-09 16:37:54 +0200

    loading msg, nodata, error catch

--- src/components/FormResults.vue
+++ src/components/FormResults.vue
@@ -2,12 +2,23 @@
   <section>
     <h2>Submitted Results</h2>
     <button @click="loadData">Load data</button>
-    <ul>
+    <p v-if="isLoading">Loading...</p>
+    <p v-else-if="!isLoading && error">
+      {{ error }}
+    </p>
+    <p v-else-if="!isLoading && (!results || results.length === 0)">
+      No stored data found. Save something to see the data.
+    </p>
+    <ul v-else-if="!isLoading && results && results.length > 0">
       <li v-for="result in results" :key="result.id">
         <h4>{{ result.userName }}</h4>
         <span>{{ result.userAge }}</span>
         <p>{{ result.infoSource }}</p>
-        <p>{{ result.interests }}</p>
+        <ul>
+          <li v-for="interest in result.interests" :key="interest[index]">
+            <p>{{ interest }}</p>
+          </li>
+        </ul>
         <p>{{ result.howLearn }}</p>
         <p>{{ result.rating }}</p>
       </li>
@@ -21,17 +32,23 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
+      error: null,
     };
   },
   methods: {
     loadData() {
-      fetch("https://vue-form-http-default-rtdb.firebaseio.com/learning.json")
+      this.isLoading = true;
+      fetch(
+        "https://vue-form-a6941-default-rtdb.europe-west1.firebasedatabase.app/learning.json"
+      )
         .then((response) => {
           if (response.ok) {
             return response.json();
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
@@ -45,10 +62,33 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          console.error(error);
+          this.error = "Something went wrong, please try again later";
         });
     },
+  },
+  mounted() {
+    this.loadData();
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+ul li {
+  display: flex;
+  align-items: center;
+  width: 40%;
+}
+li h4,
+li span,
+li p {
+  margin: 1rem;
+}
+</style>
